76abafff5d21c4f6e5b1186d8b46d5ca643452a3
Marckeyo822 <130473103+Marckeyo822@users.noreply.github.com>
2023-04-11 23:14:14 +0800

    signup

--- EPMS/src/app/landing/components/signup/signup.component.ts
+++ EPMS/src/app/landing/components/signup/signup.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupUsers: any[] = [];
+  signUpObj: any = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    idNum: '',
+    phone: '',
+    password: '',
+    confirmpassword: ''
+  }
+  constructor(){}
 
+  ngOnInit(): void {
+
+  }
+  onSignUp(){
+    this.signupUsers.push(this.signUpObj)
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+  }
 }
