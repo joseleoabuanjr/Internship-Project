629c14f54c780be42db7cb9c94f3fb87dbc71be9
leoabuan <119068420+joseleoabuanjr@users.noreply.github.com>
2023-04-11 22:02:13 +0800

    Generate Login component

--- EPMS/src/app/landing/landing.module.ts
+++ EPMS/src/app/landing/landing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule
