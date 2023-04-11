24732a70c64ff7da839e9fdafe426d29268ffd31
leoabuan <119068420+joseleoabuanjr@users.noreply.github.com>
2023-04-11 21:08:40 +0800

    Remake Files

--- EPMS/src/app/app.module.ts
+++ EPMS/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
