9d2660bcadc63fc93393607328a740739ca9f67e
Marckeyo822 <130473103+Marckeyo822@users.noreply.github.com>
2023-04-12 07:38:07 +0800

    signup

--- EPMS/src/app/page1/page1.component.ts
+++ EPMS/src/app/page1/page1.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.css']
 })
-export class Page1Component {
+export class Page1Component implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+  registerForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+    idNum: new FormControl(""),
+    phone: new FormControl(""),
+    password: new FormControl(""),
+    conPassword: new FormControl("")
+  });
+
+  registerSubmitted(){
+    console.log(this.registerForm.value);
+  }
 
 }
