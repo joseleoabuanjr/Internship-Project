44906070ff8c15e20eb7f146fd655276255dae1a
leoabuan <119068420+joseleoabuanjr@users.noreply.github.com>
2023-04-12 21:12:35 +0800

    Navbar component and styles

--- EOMS/src/app/app.module.ts
+++ EOMS/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
--- EOMS/src/app/main/main.module.ts
+++ EOMS/src/app/main/main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     MainComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
