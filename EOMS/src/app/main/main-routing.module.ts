0062054352efe02a4415d27778c46eb819188434
she33esh <carpiojr.noel.r.4935@gmail.com>
2023-04-13 17:40:36 +0800

    accounts,partners,programs, routing

--- EOMS/src/app/main/main-routing.module.ts
+++ EOMS/src/app/main/main-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { ProgramComponent } from './components/program/program.component';
 
 const routes: Routes = [
   {
@@ -9,6 +12,9 @@ const routes: Routes = [
     component: MainComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'accounts', component: AccountsComponent },
+      { path: 'partners', component: PartnersComponent },
+      { path: 'program', component: ProgramComponent },
     ]
   },
 ]
--- EOMS/src/app/main/main.module.ts
+++ EOMS/src/app/main/main.module.ts
@@ -4,12 +4,18 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from '../core/components/navbar/navbar.component';
+import { ProgramComponent } from './components/program/program.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProgramComponent,
+    PartnersComponent,
+    AccountsComponent
   ],
   imports: [
     CommonModule,
