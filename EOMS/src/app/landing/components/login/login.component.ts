47620c6fface18998428ebfba1d1ee7efef895c8
leoabuan <119068420+joseleoabuanjr@users.noreply.github.com>
2023-04-14 00:18:25 +0800

    Reactive Login Validation

--- EOMS/src/app/landing/components/login/login.component.ts
+++ EOMS/src/app/landing/components/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email:  new FormControl('', [Validators.required, Validators.email]),
+      username:  new FormControl('', [Validators.required]),
       password: new FormControl('',[Validators.required])
     });
   }
