30c2f458d5587c5766df5d302e5b14ca49cc45ac
MissGlamorazzi <mainemrry019@gmail.com>
2023-04-18 04:46:30 +0800

    Log In and Sign Up Final Flourish / Animation Prep

--- EOMS/src/app/app.component.ts
+++ EOMS/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component/*, HostBinding*/ } from '@angular/core';
+// import {
+//   trigger,
+//   state,
+//   style,
+//   animate,
+//   transition,
+// } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // animations: [
+  // i'll put some animations later
+  // ]
 })
 export class AppComponent {
   title = 'EOMS';
-}
+}/*import { animation } from '@angular/animations';*/
