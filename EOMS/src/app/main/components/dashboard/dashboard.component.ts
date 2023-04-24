7a0e37e53afa679099fa0889ab1433486459fb9f
Jason Esc <jason.escasinas.18@gmail.com>
2023-04-25 01:09:47 +0800

    Dashboard - To Do List (Unfinished)

--- EOMS/src/app/main/components/dashboard/dashboard.component.ts
+++ EOMS/src/app/main/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from "./item";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,5 +13,34 @@ export class DashboardComponent {
     {
       this.status = !this.status;       
     }
+  
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item: Item) {
+  this.allItems.splice(this.allItems.indexOf(item), 1);
+}
 
 }
