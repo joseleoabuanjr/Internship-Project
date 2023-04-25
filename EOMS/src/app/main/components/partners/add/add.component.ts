1d534569f8aa5b756ee4522d641c1d147e37a603
Jason Esc <jason.escasinas.18@gmail.com>
2023-04-25 14:57:03 +0800

    Update - Add Partners

--- EOMS/src/app/main/components/partners/add/add.component.ts
+++ EOMS/src/app/main/components/partners/add/add.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent {
 
+export class AddComponent implements OnInit {
+  addPartner!: FormGroup;
+
+  constructor( private router: Router){ /* TODO document why this constructor is empty */ }
+
+  ngOnInit(): void {
+    this.addPartner = new FormGroup({
+      start: new FormControl('',[Validators.required]),
+      end: new FormControl('',[Validators.required]),
+      title: new FormControl('',[Validators.required]),
+      place: new FormControl('',[Validators.required]),
+      banner: new FormControl('',[Validators.required]),
+      uploadFiles: new FormControl('',[Validators.required]),
+      details: new FormControl('',[Validators.required]),
+      leadAndMembers: new FormControl('',[Validators.required]),
+      participants: new FormControl('',[Validators.required]),
+    });
+  }
+  url="./assets/images/cict.png"
+
+  onselectFile(banner: any){
+    if(banner.target.files){
+      let reader = new FileReader();
+      reader.readAsDataURL(banner.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
+  }
+  dis: any
+  show(){
+    this.dis ="inline"
+  }
+
+  partners: any[]= []
+  submit(): void {
+    console.log('Saved: ' + JSON.stringify(this.addPartner.value));
+
+    this.partners.push(this.addPartner.value)
+
+    if(this.addPartner.value.start <= this.addPartner.value.end){
+      // TODO document why this block is empty
+    }
+
+  }
 }
