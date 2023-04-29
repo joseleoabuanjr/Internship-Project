2d7401b5cd6f1a98710153c101e9e5ac68c53ced
Marckeyo822 <130473103+Marckeyo822@users.noreply.github.com>
2023-04-29 21:31:22 +0800

    generate-reports

--- EOMS/src/app/main/components/reports/reports.component.ts
+++ EOMS/src/app/main/components/reports/reports.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-reports',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class ReportsComponent {
 
+  @ViewChild('print', {static:true}) el!: ElementRef<HTMLImageElement>
+
+  makePdf(){
+    html2canvas(this.el.nativeElement).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png')
+
+      const pdf = new jsPDF( 'p', 'mm', 'a4')
+
+      pdf.setFontSize(20)
+
+      const imageProps = pdf.getImageProperties(imgData)
+
+      pdf.addImage(imgData, 'PNG/JPG', 0, 0, 0 , 0 )
+
+      pdf.save('sample.pdf')
+    })
+  }
 }
