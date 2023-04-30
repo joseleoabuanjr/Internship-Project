c6805e4dfdc4c377706746771960a7ad607137d9
MissGlamorazzi <mainemrry019@gmail.com>
2023-04-30 23:04:09 +0800

    data service - createPrograms included

--- EOMS/src/app/core/services/data.service.ts
+++ EOMS/src/app/core/services/data.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { Users } from '../models/users';
 import { DataUser } from 'src/app/core/models/data';
+import { Programs } from '../models/programs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,14 @@ export class DataService {
       this.setToken(Users.data.account_id);
       return Users;
     }));
+  }
+
+  createPrograms(program: Programs){
+    return this.http.post<Programs[]>(this.baseUrl+'programs.php', program);
+  }
+
+  getPrograms() {
+    return this.http.get<DataUser>(this.baseUrl+'view.php');
   }
 
   //Token-------------------------------
